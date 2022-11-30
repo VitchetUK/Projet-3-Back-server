cf0df45f9040630e56b7eabf713688b17f867361
Vitchet Uk <vitchet.uk@hotmail.com>
2022-11-30 10:39:09 +0100

    added routes

---
+++ routes/profile.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const isAuthenticated = require("../middlewares/jwt.middleware");
+const User = require("../models/User.model");
