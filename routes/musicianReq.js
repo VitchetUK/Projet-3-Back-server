b56754504703a33927b8f541c7f9acba21684032
Céline Poinson <cpoinson@hotmail.fr>
2022-11-30 11:07:06 +0100

    add musician routes

--- routes/musicianReq.js
+++ routes/musicianReq.js
@@ -4,3 +4,19 @@ const mongoose = require("mongoose");
 const isAuthenticated = require("../middlewares/jwt.middleware");
 const User = require("../models/User.model");
 const Musician = require("../models/Musician.model");
+
+router.get("/allMusicianRequest", async (req, res, next) => {
+  res.status(200).json(await Musician.find());
+});
+
+router.get("/allMusicianRequest/:id", async (req, res, next) => {
+  try {
+    const oneMusicianRequest = await Musician.findById(req.params.id);
+
+    res.status(200).json(oneMusicianRequest);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
