e0659fb63d6ddac8610e8c928282ce7a654f6c95
Céline Poinson <cpoinson@hotmail.fr>
2022-11-30 16:31:02 +0100

    add changes

--- routes/musicianReq.js
+++ routes/musicianReq.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const isAuthenticated = require("../middlewares/jwt.middleware");
 const User = require("../models/User.model");
 const Musician = require("../models/Musician.model");
+const { route } = require("./auth");
 
 router.get("/allMusicianRequest", async (req, res, next) => {
   res.status(200).json(await Musician.find());
@@ -17,6 +18,28 @@ router.get("/allMusicianRequest/:id", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+router.post("/allMusicianRequest", async (req, res, next) => {
+  const {
+    user,
+    instruments,
+    musicStyle,
+    city,
+    experience,
+    description,
+    availability,
+  } = req.body;
+  const musician = await Musician.create({
+    user,
+    instruments,
+    musicStyle,
+    city,
+    experience,
+    description,
+    availability,
+  });
+  res.status(201).json(musician);
 });
 
 module.exports = router;
