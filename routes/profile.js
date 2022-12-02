3ba4500a6191d5951404865b6ae000ea8bb764a3
Vitchet Uk <vitchet.uk@hotmail.com>
2022-12-02 12:39:40 +0100

    idr what i did

--- routes/profile.js
+++ routes/profile.js
@@ -3,58 +3,63 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const isAuthenticated = require("../middlewares/jwt.middleware");
 const User = require("../models/User.model");
+const uploader = require("../config/cloudinary");
 
 router.get("/", isAuthenticated, async (req, res) => {
   try {
     console.log(req.payload);
 
     const id = req.payload.id;
-    const user = await User.findById(id, { name: 1, email: 1 });
+    const user = await User.findById(id, { password: 0 });
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json(error);
   }
 });
 
-router.post("/", isAuthenticated, async (req, res) => {
-  try {
-    const userId = req.payload.id;
-    const user = req.payload;
-
-    const {
-      name,
-      username,
-      email,
-      phone,
-      age,
-      twitter,
-      instagram,
-      displayEmail,
-    } = req.body;
-
+router.patch(
+  "/",
+  uploader.single("picture"),
+  isAuthenticated,
+  async (req, res) => {
     try {
-      let userUpdate;
+      const userId = req.payload.id;
+      let user = req.payload;
 
-      userUpdate = await User.findByIdAndUpdate(userId, {
-        name,
-        username,
-        email,
-        phone,
-        age,
-        twitter,
-        instagram,
-        displayEmail,
-      });
+      const { name, username, phone, age, twitter, instagram, displayEmail } =
+        req.body;
 
-      user = userUpdate;
-      await userUpdate.save();
-      res.redirect("/");
+      try {
+        let userUpdate;
+        let picture;
+        if (req.file) {
+          picture = req.file.path;
+        }
+
+        userUpdate = await User.findByIdAndUpdate(
+          userId,
+          {
+            name,
+            username,
+            phone,
+            age,
+            twitter,
+            instagram,
+            displayEmail,
+            picture,
+          },
+          { new: true }
+        );
+        res.status(200).json(userUpdate);
+        //user = userUpdate;
+        //await userUpdate.save();
+      } catch (error) {
+        res.status(400).json({ error: error.message });
+      }
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
   }
-});
+);
 
 module.exports = router;
